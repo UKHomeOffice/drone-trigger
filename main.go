f61a09e8fc2acf50c7bd324e1ff58685ebbaa9f6
Vaidas Jablonskis <jablonskis@gmail.com>
2016-08-15 16:47:20 +0100

    Add initial code

---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+
+	"github.com/drone/drone/model"
+	"github.com/urfave/cli"
+)
+
+// Version is set at compile time, passing -ldflags "-X main.Version=<build version>"
+var Version string
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "drone-trigger"
+	app.Version = Version
+	app.Usage = "trigger drone builds or deployments"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:   "verbose",
+			Usage:  "verbose output",
+			EnvVar: "VERBOSE,PLUGIN_VERBOSE",
+		},
+		cli.BoolFlag{
+			Name:   "fork",
+			Usage:  "fork an existing build - drone assigns a new build number",
+			EnvVar: "FORK,PLUGIN_FORK",
+		},
+		cli.StringFlag{
+			Name:   "drone-server, s",
+			Usage:  "drone server `URL`",
+			EnvVar: "DRONE_SERVER,PLUGIN_DRONE_SERVER",
+		},
+		cli.StringFlag{
+			Name:   "drone-token, t",
+			Usage:  "drone auth `TOKEN`",
+			EnvVar: "DRONE_TOKEN,PLUGIN_DRONE_TOKEN",
+		},
+		cli.StringFlag{
+			Name:   "repo, r",
+			Usage:  "`REPO`, eg. foo/bar",
+			EnvVar: "REPO,PLUGIN_REPO",
+		},
+		cli.StringFlag{
+			Name:   "commit, c",
+			Usage:  "filter by commit sha",
+			EnvVar: "FILTER_COMMIT,PLUGIN_COMMIT",
+		},
+		cli.StringFlag{
+			Name:   "tag",
+			Usage:  "filter by tag",
+			EnvVar: "FILTER_TAG,PLUGIN_TAG",
+		},
+		cli.StringFlag{
+			Name:   "branch, b",
+			Usage:  "filter by branch",
+			EnvVar: "FILTER_BRANCH,PLUGIN_BRANCH",
+		},
+		cli.StringFlag{
+			Name:   "status",
+			Usage:  "filter by build status",
+			EnvVar: "FILTER_STATUS,PLUGIN_STATUS",
+			Value:  "success",
+		},
+		cli.IntFlag{
+			Name:   "number",
+			Usage:  "filter by build number",
+			EnvVar: "FILTER_NUMBER,PLUGIN_NUMBER",
+		},
+		cli.StringFlag{
+			Name:   "event",
+			Usage:  "filter by trigger event",
+			EnvVar: "FILTER_EVENT,PLUGIN_EVENT",
+		},
+		cli.StringFlag{
+			Name:   "deploy-to, d",
+			Usage:  "environment to deploy to, if set a deployment event will be triggered",
+			EnvVar: "DEPLOY_TO,PLUGIN_DEPLOY_TO",
+		},
+		cli.StringSliceFlag{
+			Name:   "param, p",
+			Usage:  "custom parameters to include in the trigger in KEY=value format",
+			EnvVar: "PARAMS,PLUGIN_PARAMS",
+		},
+	}
+
+	app.Action = run
+	app.Run(os.Args)
+}
+
+func run(ctx *cli.Context) error {
+	// Exit if required flags are not set
+	if !ctx.IsSet("drone-server") {
+		cli.ShowAppHelp(ctx)
+		return cli.NewExitError("error: drone server is not set", 3)
+	}
+	if !ctx.IsSet("drone-token") {
+		cli.ShowAppHelp(ctx)
+		return cli.NewExitError("error: drone token is not set", 3)
+	}
+	if !ctx.IsSet("repo") {
+		cli.ShowAppHelp(ctx)
+		return cli.NewExitError("error: repo is not set", 3)
+	}
+	if (ctx.IsSet("tag") && (ctx.IsSet("branch") || ctx.IsSet("commit"))) || (ctx.IsSet("branch") && ctx.IsSet("commit")) {
+		return cli.NewExitError("error: tag, branch or commit cannot be set at the same time, pick one filter", 3)
+	}
+
+	c := newDroneClient(ctx)
+	build, err := findBuild(c, ctx)
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+	if build == nil {
+		return cli.NewExitError("No previous builds found", 1)
+	}
+
+	params := parsePairs(ctx.StringSlice("param"))
+	newBuild := &model.Build{}
+	owner, repo, err := parseRepo(ctx.String("repo"))
+	if ctx.IsSet("deploy-to") {
+		b, err := c.Deploy(owner, repo, build.Number, ctx.String("deploy-to"), params)
+		if err != nil {
+			return cli.NewExitError(err.Error(), 1)
+		}
+		newBuild = b
+	} else {
+		if ctx.IsSet("fork") {
+			params["fork"] = "true"
+		}
+		b, err := c.BuildStart(owner, repo, build.Number, params)
+		if err != nil {
+			return cli.NewExitError(err.Error(), 1)
+		}
+		newBuild = b
+	}
+
+	newBuildURL := path.Join(ctx.String("drone-server"), ctx.String("repo"), strconv.Itoa(newBuild.Number))
+	fmt.Fprintf(os.Stderr, "Follow new build status at: %s\n", newBuildURL)
+
+	if ctx.Bool("verbose") {
+		j, err := json.MarshalIndent(newBuild, "", "  ")
+		if err != nil {
+			return cli.NewExitError(err.Error(), 1)
+		}
+		fmt.Println(string(j))
+	}
+	return nil
+}
---
+++ util.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	drone "github.com/drone/drone/client"
+	"github.com/drone/drone/model"
+	"github.com/urfave/cli"
+)
+
+// findBuild requests an array of previous repo builds from drone API and finds
+// the one that matches given filters
+func findBuild(c drone.Client, ctx *cli.Context) (*model.Build, error) {
+	user, repo, err := parseRepo(ctx.String("repo"))
+	if err != nil {
+		return nil, err
+	}
+	builds, err := c.BuildList(user, repo)
+	if err != nil {
+		return nil, err
+	}
+	for _, b := range builds {
+		if match(ctx, b) {
+			return b, nil
+		}
+	}
+	return nil, nil
+}
+
+// match returns first build that matches cli filter flags
+func match(ctx *cli.Context, build *model.Build) bool {
+	if ctx.String("status") != build.Status {
+		return false
+	}
+	if ctx.IsSet("event") && ctx.String("event") != build.Event {
+		return false
+	}
+	// Build number always takes precedence
+	if ctx.IsSet("number") {
+		if ctx.Int("number") == build.Number {
+			return true
+		}
+		return false
+	}
+	if ctx.IsSet("commit") {
+		if ctx.String("commit") == build.Commit {
+			return true
+		}
+		return false
+	}
+	if ctx.IsSet("tag") {
+		if "refs/tags/"+ctx.String("tag") == build.Ref {
+			return true
+		}
+		return false
+	}
+	if ctx.String("branch") == build.Branch {
+		return true
+	}
+	// Return latest successful build if no specific filters are set
+	return true
+}
+
+func newDroneClient(ctx *cli.Context) drone.Client {
+	return drone.NewClientToken(ctx.String("drone-server"), ctx.String("drone-token"))
+}
+
+// parseRepo parses a owner/repo string into two parts
+func parseRepo(str string) (owner, repo string, err error) {
+	var parts = strings.Split(str, "/")
+	if len(parts) != 2 {
+		err = fmt.Errorf("error: invalid or missing repository. eg octocat/hello-world")
+		return
+	}
+	owner = parts[0]
+	repo = parts[1]
+	return
+}
+
+// parsePairs parses an array of KEY=VALUE strings into a map
+func parsePairs(p []string) map[string]string {
+	params := map[string]string{}
+	for _, i := range p {
+		parts := strings.Split(i, "=")
+		if len(parts) != 2 {
+			continue
+		}
+		params[parts[0]] = parts[1]
+	}
+	return params
+}
