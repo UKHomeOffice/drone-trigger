65eb8b39cd447f26b5512c74676cf5c35f35f19c
Vaidas Jablonskis <jablonskis@gmail.com>
2017-08-08 12:35:14 +0300

    Port to support drone v0.8+ and remove fork flag
    Fork flag is being removed from the API. After that, every build trigger
    will create a new job.

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path"
 	"strconv"
+	"strings"
 
-	"github.com/drone/drone/model"
+	"github.com/drone/drone-go/drone"
 	"github.com/urfave/cli"
+	"golang.org/x/oauth2"
 )
 
 // Version is set at compile time, passing -ldflags "-X main.Version=<build version>"
@@ -26,11 +29,6 @@ func main() {
 			Name:   "verbose",
 			Usage:  "verbose output",
 			EnvVar: "VERBOSE,PLUGIN_VERBOSE",
-		},
-		cli.BoolFlag{
-			Name:   "fork",
-			Usage:  "fork an existing build - drone assigns a new build number",
-			EnvVar: "FORK,PLUGIN_FORK",
 		},
 		cli.StringFlag{
 			Name:   "drone-server, s",
@@ -99,18 +97,43 @@ func main() {
 	app.Run(os.Args)
 }
 
+// Trigger represent drone trigger.
+type Trigger struct {
+	client drone.Client
+}
+
+func newTrigger(ctx *cli.Context) *Trigger {
+	oauthCfg := &oauth2.Config{}
+	httpClient := oauthCfg.Client(
+		context.Background(),
+		&oauth2.Token{
+			AccessToken: ctx.String("drone-token"),
+		},
+	)
+
+	return &Trigger{
+		client: drone.NewClient(ctx.String("drone-server"), httpClient),
+	}
+}
+
 func run(ctx *cli.Context) error {
 	// Exit if required flags are not set
 	if !ctx.IsSet("drone-server") && !isAnyEnvSet("DRONE_SERVER", "PLUGIN_DRONE_SERVER") {
-		cli.ShowAppHelp(ctx)
+		if err := cli.ShowAppHelp(ctx); err != nil {
+			return cli.NewExitError(err, 1)
+		}
 		return cli.NewExitError("error: drone server is not set", 3)
 	}
 	if !ctx.IsSet("drone-token") && !isAnyEnvSet("DRONE_TOKEN", "PLUGIN_DRONE_TOKEN") {
-		cli.ShowAppHelp(ctx)
+		if err := cli.ShowAppHelp(ctx); err != nil {
+			return cli.NewExitError(err, 1)
+		}
 		return cli.NewExitError("error: drone token is not set", 3)
 	}
 	if !ctx.IsSet("repo") && !isAnyEnvSet("REPO", "PLUGIN_REPO") {
-		cli.ShowAppHelp(ctx)
+		if err := cli.ShowAppHelp(ctx); err != nil {
+			return cli.NewExitError(err, 1)
+		}
 		return cli.NewExitError("error: repo is not set", 3)
 	}
 
@@ -136,8 +159,8 @@ func run(ctx *cli.Context) error {
 		return cli.NewExitError("error: tag, branch, commit or deployed-to cannot be set at the same time, pick one filter", 3)
 	}
 
-	c := newDroneClient(ctx)
-	build, err := findBuild(c, ctx)
+	t := newTrigger(ctx)
+	build, err := t.findBuild(ctx)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 1)
 	}
@@ -146,19 +169,20 @@ func run(ctx *cli.Context) error {
 	}
 
 	params := parsePairs(ctx.StringSlice("param"))
-	newBuild := &model.Build{}
+	var newBuild *drone.Build
 	owner, repo, err := parseRepo(ctx.String("repo"))
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
 	if ctx.IsSet("deploy-to") || isAnyEnvSet("DEPLOY_TO", "PLUGIN_DEPLOY_TO") {
-		b, err := c.Deploy(owner, repo, build.Number, ctx.String("deploy-to"), params)
+		b, err := t.client.Deploy(owner, repo, build.Number, ctx.String("deploy-to"), params)
 		if err != nil {
 			return cli.NewExitError(err.Error(), 1)
 		}
 		newBuild = b
 	} else {
-		if ctx.IsSet("fork") || isAnyEnvSet("DRONE_TOKEN", "PLUGIN_DRONE_TOKEN") {
-			params["fork"] = "true"
-		}
-		b, err := c.BuildStart(owner, repo, build.Number, params)
+		b, err := t.client.BuildStart(owner, repo, build.Number, params)
 		if err != nil {
 			return cli.NewExitError(err.Error(), 1)
 		}
@@ -176,4 +200,104 @@ func run(ctx *cli.Context) error {
 		fmt.Println(string(j))
 	}
 	return nil
+}
+
+// findBuild requests an array of previous repo builds from drone API and finds
+// the one that matches given filters
+func (t *Trigger) findBuild(ctx *cli.Context) (*drone.Build, error) {
+	user, repo, err := parseRepo(ctx.String("repo"))
+	if err != nil {
+		return nil, err
+	}
+	builds, err := t.client.BuildList(user, repo)
+	if err != nil {
+		return nil, err
+	}
+	for _, b := range builds {
+		if match(ctx, b) {
+			return b, nil
+		}
+	}
+	return nil, nil
+}
+
+// match returns true for the first build that matches cli filter flags.
+func match(ctx *cli.Context, build *drone.Build) bool {
+	if ctx.String("status") != build.Status {
+		return false
+	}
+	if ctx.IsSet("event") && ctx.String("event") != build.Event {
+		return false
+	}
+	// Build number always takes precedence
+	if ctx.IsSet("number") {
+		if ctx.Int("number") == build.Number {
+			return true
+		}
+		return false
+	}
+	if ctx.IsSet("commit") {
+		if ctx.String("commit") == build.Commit {
+			return true
+		}
+		return false
+	}
+	if ctx.IsSet("tag") {
+		if "refs/tags/"+ctx.String("tag") == build.Ref {
+			return true
+		}
+		return false
+	}
+	if ctx.IsSet("deployed-to") {
+		if ctx.String("deployed-to") == build.Deploy {
+			return true
+		}
+		return false
+	}
+	// Matching on branch does not make sense for pull_request events, because
+	// the branch for PR events is the base branch.
+	if ctx.IsSet("branch") {
+		if ctx.String("branch") == build.Branch && build.Event != "pull_request" {
+			return true
+		}
+		return false
+	}
+	// Return latest successful build if no specific filters are set
+	return true
+}
+
+// parseRepo parses a owner/repo string into two parts
+func parseRepo(s string) (owner, repo string, err error) {
+	var parts = strings.Split(s, "/")
+	if len(parts) != 2 {
+		err = fmt.Errorf("error: invalid or missing repository. eg octocat/hello-world")
+		return
+	}
+	owner = parts[0]
+	repo = parts[1]
+	return
+}
+
+// parsePairs parses an array of KEY=VALUE strings into a map
+func parsePairs(p []string) map[string]string {
+	params := map[string]string{}
+	for _, i := range p {
+		parts := strings.Split(i, "=")
+		if len(parts) != 2 {
+			continue
+		}
+		params[parts[0]] = parts[1]
+	}
+	return params
+}
+
+// isAnyEnvSet checks if any of given environment vars is set and returns a boolean
+func isAnyEnvSet(vars ...string) bool {
+	for _, v := range vars {
+		_, r := os.LookupEnv(v)
+		if r {
+			return r
+		}
+	}
+	return false
 }
